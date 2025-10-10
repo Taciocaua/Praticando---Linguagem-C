a0ff0ca818a52db464812c174bca054591d103c6
Taciocaua <tccaua@outlook.com>
2025-10-10 11:04:05 -0300

    Aulas C

---
+++ aula.c
@@ -0,0 +1,17 @@
+//Aulas de variaveis. 
+//OBS: Em C, o " ; " serve para finalizar o comando.
+#include <stdio.h>
+//Declarando variáveis ↓
+int main(){;
+     
+    int idade;
+
+//Função para escrever ↓ (printf)    
+    printf("Quantos anos voce tem? ");
+ 
+    scanf("%d",&idade);
+
+    printf("Sua idade e %d", idade);
+
+    return 0;
+}//Fim do bloco.    
