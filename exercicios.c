552d4e4bbf94c1b9a50556c7f63dcb05f2c0958a
Taciocaua <tccaua@outlook.com>
2025-10-10 11:38:50 -0300

    aulas em c

---
+++ exercicios.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+//Declarando variáveis ↓
+int main(){;
+//Main → função principal
+
+int numero; 
+//Declarando variável do tipo inteiro
+
+printf("Digite um número: ");
+//Pedindo para o usuário digitar um número
+
+scanf("%d", &numero);
+//Lendo o valor da variável
+
+printf("O número digitado foi: %d\n", numero);
+//Imprimindo o valor da variável  
+  
+return 0;
+}
