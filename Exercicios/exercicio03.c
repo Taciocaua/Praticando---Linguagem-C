c818dc3813239eab98bcf36b4d07cb836a756504
Taciocaua <tccaua@outlook.com>
2025-10-10 11:54:57 -0300

    exercicios em c

---
+++ Exercicios/exercicio03.c
@@ -0,0 +1,19 @@
+//Leia um número e imprima o resultado do quadrado deste número ↓
+
+#include <stdio.h>
+//Declarando variáveis ↓
+int main(){;
+
+int numero, quadrado;
+//Entrada de dados ↓
+
+printf("DIgite um numero: ");
+scanf("%d",&numero);
+//Processamento ↓
+
+quadrado = numero * numero;
+//Saída de dados ↓
+
+printf("O quadrado do numero e: %d",quadrado);
+return 0;
+} 
