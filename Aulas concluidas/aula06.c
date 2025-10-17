b498ee2f9cf862d506a4943b4b4d7ade5a26dc4f
Taciocaua <tccaua@outlook.com>
2025-10-17 11:02:48 -0300

    aulas em c

---
+++ Aulas concluidas/aula06.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Faça um progama, no qual receba e some 5 numeros 
+inteiros e apresenta a soma total */
+
+int main(){
+  
+//Declarando variáveis ↓
+int n,soma = 0;
+
+//Entrada de dados ↓
+for(int i = 0; i < 5; i++){
+    printf("Digite o numero: ");
+    scanf("%i",&n);
+
+//Processamentos de dados ↓
+soma = soma + n;
+
+}
+//Saida de dados ↓
+printf("A soma dos numeros é %i",soma);
+
+return 0;
+}
