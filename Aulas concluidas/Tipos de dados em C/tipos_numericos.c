51964449a0e5ebb42538e187c4a51676a2d32721
Taciocaua <tccaua@outlook.com>
2025-10-24 10:52:02 -0300

    aulas em c

---
+++ Aulas concluidas/Tipos de dados em C/tipos_numericos.c
@@ -0,0 +1,21 @@
+/*
+Tipos numericos em C:
+int - inteiro
+float - real com casa decimal
+double - real com maior precisão(mais casas decimais)
+*/
+
+#include <stdio.h>
+int main(){;
+
+//inteiro ↓
+int numero_inteiro; //1,1000,20,-05
+
+//float ↓
+float numero_real; //1.5,3.14,-10.25,0.001(Numeros com casa decimal)
+
+//double ↓ (%lf)
+double numero; //1.123456789,3.1415926535(Numeros com maior precisão, com mais casas decimais)
+
+return 0;
+}
